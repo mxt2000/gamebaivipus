7d2388fed8cbcecce8997279ee5b6d5e9444a499
rinnguyen237 <rinnguyen237@gmail.com>
2021-08-04 12:46:23 -0400

    upate

---
+++ config/database.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+	'url': 'mongodb://127.0.0.1:27017',
+	'options': {
+		//'user':   'root',
+		//'pass':   '',
+		'dbName': 'server', // red
+		'useNewUrlParser': true,
+		'useUnifiedTopology': true,
+		//'autoIndex':       false,
+	},
+};
